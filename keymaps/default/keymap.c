62d9e9b92dd8e6504055daadb8b4a4a4f21b2bb7
Francesco Prette <francesco.prette@gmail.com>
2025-06-23 16:48:37 +0200

    Changed the layout

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │CTL│Win│ALT│NO │SPA│SPA│ │SPA│NO │NO │NO │NO │END│
      * └───┴───┴───┴───┴───┴───┘ └───┴───┴───┴───┴───┴───┘
      */
-    [1] = LAYOUT_1(
+    [1] = LAYOUT(
         KC_GRV,             KC_F1,              KC_F2,                  KC_F3,              KC_F4,             KC_F5,       KC_F6,        KC_F7,   KC_F8,                KC_F9,    KC_F10,    KC_DEL,
         KC_TAB,             KC_F11,             KC_F12,                 KC_E,               KC_R,              KC_T,        KC_Y,         KC_U,    KC_I,                 KC_LBRC,  KC_RBRC,   KC_BSLS,
         KC_CAPS_LOCK,       KC_A,               KC_S,                   KC_D,               KC_F,              KC_G,        KC_H,         KC_J,    KC_K,                 KC_L,     KC_QUOT,   KC_ENT,
